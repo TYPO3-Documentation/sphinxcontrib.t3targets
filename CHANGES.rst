77c0140e6756712ec4e85cec227c023ad0ea3e4a
Martin Bless <martin.bless@mbless.de>
2019-12-09 16:49:40 +0100

    Set version to 0.3.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,9 +1,19 @@
+Release 0.3.0 (Dec 9, 2019)
+===========================
+
+* link line numbers to *.rst.txt
+
+
 
 Release 0.2.1 (May 28, 2019)
 ============================
+
+* ...
+
 
 
 Version 0.1 (2014-12-10)
 ========================
 
 * Initial upload to PyPi
+
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/development.html#single-sourcing-the-version
-    version='0.2.1',
+    version='0.3.0',
 
     description='Implement Sphinx directive "ref-targets-list"',
     long_description=long_description,
